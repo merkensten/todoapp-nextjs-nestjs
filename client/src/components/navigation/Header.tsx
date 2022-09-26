d21ebe8e6cd6134f40dddf51b10c5dbbb7c72369
Mattis <m.erkensten@icloud.com>
2022-09-26 13:45:32 +0200

    Header och startsida design påbörjad

--- client/src/components/navigation/Header.tsx
+++ client/src/components/navigation/Header.tsx
@@ -2,21 +2,61 @@ import React from 'react';
 import styles from '../../styles/components/navigation/Header.module.scss';
 import { useRouter } from 'next/router';
 import { AuthContext } from '../../context/AuthContext';
+import Link from 'next/link';
 
-function Header() {
-  const { logoutUser, isUserAuthenticated } = React.useContext(AuthContext);
+function GuestHeader() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles.header_logo}>Todo Next.js Nest Js App</a>
+      </Link>
+      <nav>
+        <Link href="/login">
+          <button className="btn-secondary">Logga in</button>
+        </Link>
+        <Link href="/signup">
+          <button className="btn-primary">Registrera</button>
+        </Link>
+      </nav>
+    </header>
+  );
+}
+function AppHeader() {
+  const { logoutUser } = React.useContext(AuthContext);
   const router = useRouter();
   function logoutUserFunc() {
     logoutUser();
     router.push('/login');
   }
   return (
-    <div>
-      <h1>Header</h1>
-      {isUserAuthenticated && (
-        <button onClick={logoutUserFunc}>Logga ut</button>
-      )}
-    </div>
+    <header className={styles.header}>
+      <Link href="/app">
+        <a className={styles.header_logo}>Todo Next.js Nest Js App</a>
+      </Link>
+      <nav>
+        <Link href="/app/konto">
+          <button className="btn-secondary">Mitt konto</button>
+        </Link>
+        <button className="btn-primary" onClick={logoutUserFunc}>
+          Logga ut
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+function Header() {
+  const { logoutUser, isUserAuthenticated } = React.useContext(AuthContext);
+  // const router = useRouter();
+  // function logoutUserFunc() {
+  //   logoutUser();
+  //   router.push('/login');
+  // }
+  return (
+    <>
+      {isUserAuthenticated && <AppHeader />}
+      {!isUserAuthenticated && <GuestHeader />}
+    </>
   );
 }
 
